3d322fb42a5cfed98be709e07cbbad450b2998c6
Manish <chandrajob365@gmail.com>
2017-12-08 08:09:38 +0530

    Server configration

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+server.listen(3000, () => {
+  console.log('Server running @ : ', 3000)
+})
+
+io.on('connection', socket => {
+  console.log('New connection : ', socket.id)
+  socket.emit('created', socket.id)
+  socket.on('message', message => {
+    console.log('[app.js] Client said : ', message)
+    socket.broadcast.emit('message', message)
+  })
+})
